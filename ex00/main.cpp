a6b074a7f3b0b47504c012020feb31cf7ecdc356
MiguelTolino <mmateo-t@student.42madrid.com>
2022-05-25 11:45:04 +0200

    ex00 done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmateo-t <mmateo-t@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 17:42:48 by mmateo-t          #+#    #+#             */
-/*   Updated: 2022/05/03 19:04:55 by mmateo-t         ###   ########.fr       */
+/*   Updated: 2022/05/25 11:44:37 by mmateo-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,26 @@
 
 int main(int argc, char const *argv[])
 {
-	ClapTrap c1("Juanito");
-	ClapTrap c2("Pepito");
+ 	ClapTrap c1("Juanito");
+	ClapTrap c2;
 	ClapTrap c3("Marianito");
-	ClapTrap c4;
+	ClapTrap c4 = c1;
 
+	std::cout << std::endl;
 	c1.takeDamage(50);
 	c2.takeDamage(50);
 	c3.takeDamage(50);
 	c4.takeDamage(50);
-
+	std::cout << std::endl;
 	c1.beRepaired(20);
 	c2.beRepaired(20);
 	c3.beRepaired(20);
 	c4.beRepaired(20);
-
+	std::cout << std::endl;
 	c1.attack("Skag");
 	c2.attack("Rakk");
 	c3.attack("Psycho");
 	c4.attack("Psycho");
-
+	std::cout << std::endl;
 	return 0;
 }
